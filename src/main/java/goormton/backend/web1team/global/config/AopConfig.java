e306a6fede09539572e92f4d75c52040c397d837
T-ferret <hs381717@gmail.com>
2025-04-01 18:18:59 +0900

    Aop 선언

---
+++ src/main/java/goormton/backend/web1team/global/config/AopConfig.java
@@ -0,0 +1,14 @@
+package goormton.backend.web1team.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class AopConfig {
+
+//    @Bean
+//    public LoggingAspect loggingAspect() {
+//        return new LoggingAspect();
+//    }
+}
